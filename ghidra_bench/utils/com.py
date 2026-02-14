a45a275ce7ff0816e8c4a6eb7ceec2c45f8d4b23
Lurpigi <luigiestebant@gmail.com>
2026-02-14 17:34:11 +0100

    big run

--- ghidra_bench/utils/com.py
+++ ghidra_bench/utils/com.py
@@ -552,7 +552,7 @@ def fetch_decompiler_prs():
             pr_numbers = [str(item['number']) for item in items]
             print(f"[GITHUB] Found {len(pr_numbers)} PRs: {pr_numbers}")
             # pr_numbers  # 5554, '8834']  # pr_numbers
-            return ['8628', '8587', '7253', '6722', '6718']
+            return ['8628', '8587', '8161', '7253', '6722']
             # return ['3299', '8597']
         elif response.status_code == 403:
             print("[WARN] GitHub API rate limit exceeded or access denied.")
